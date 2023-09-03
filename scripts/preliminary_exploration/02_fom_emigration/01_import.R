f5ea18cd9122756f09b6ff61bd0f8586384dc4a0
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-09-03 23:00:37 +0200

    Add sources to input script

--- scripts/preliminary_exploration/02_fom_emigration/01_import.R
+++ scripts/preliminary_exploration/02_fom_emigration/01_import.R
@@ -3,6 +3,10 @@
 
 # Net international migration
 net_int_migration_raw <- read.csv(here("data", "net_int_migration_raw.csv"))
+
 # Sources
 # Croatia: stan-2021-2-1_tablice-eng.xlsx - stan-2023-2-1_tablice_eng.xlsx
+# Czech Republic: online
 # Poland: main_directions_of_emigration_and_immigration_in_the_years_1966-2020_for__permanent_residence.xlsx
+# Romania: online
+# Slovenia: online
