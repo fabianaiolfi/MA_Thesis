249350771974dcb2c74c95ffcaca91a28341bf30
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-09-12 13:51:01 +0200

    Update 03_figures.R
    Make plots larger

--- scripts/preliminary_exploration/03_fom_emigration/03_figures.R
+++ scripts/preliminary_exploration/03_fom_emigration/03_figures.R
@@ -3,7 +3,7 @@
 
 ggplot(net_int_migration, aes(y = Emigration, x = Year)) +
   geom_col(fill = "#666666") +
-  facet_wrap(vars(Country), scales = "free_y") +
+  facet_wrap(vars(Country), scales = "free_y", ncol = 2) +
   geom_vline(aes(xintercept = EU_Accession), color = "blue", size = 1) +
   ylab("Emigration") +
   # ggtitle("International Emigration and EU Accession") +
