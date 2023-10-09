b1e475fbbaee55c87df37ad6682dd92cefd79e01
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-10-09 15:06:24 +0200

    Update 02_modelling.R
    Adjust lag to be same as primary school duration in Croatia

--- scripts/preliminary_exploration/07_schools_emigration/02_modelling.R
+++ scripts/preliminary_exploration/07_schools_emigration/02_modelling.R
@@ -7,7 +7,7 @@ summary(ols)
 
 # Lagged Panel Data
 hr_schools_emigration_pdata <- pdata.frame(hr_schools_emigration, index = c("NUTS", "year"))
-hr_schools_emigration_pdata$net_migration_lag <- lag(hr_schools_emigration_pdata$net_migration, 7)
+hr_schools_emigration_pdata$net_migration_lag <- lag(hr_schools_emigration_pdata$net_migration, 8)
 
 plm_model <- plm(schools ~ net_migration_lag,
                  data = hr_schools_emigration_pdata,
@@ -23,3 +23,12 @@ plot(hr_schools_emigration$net_migration, hr_schools_emigration$schools)
 # Lagged Panel Data
 df_plot <- as.data.frame(hr_schools_emigration_pdata)
 plot(as.numeric(df_plot$net_migration_lag), as.numeric(df_plot$schools))
+
+ggplot(df_plot, aes(net_migration_lag, schools)) +            
+  geom_point() +
+  stat_smooth(method = "lm", 
+              formula = y ~ x, 
+              geom = "smooth") +
+  ylab("Number of Schools in NUTS3 Region") +
+  xlab("Net Migration in NUTS3 Region (Lag: 8 years)") +
+  theme_minimal()
