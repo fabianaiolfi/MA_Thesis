626afeac657b6be073c711b4dc82bff35f6392c8
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-11-02 20:38:24 +0100

    Update script

--- scripts/01_anti_incumbent_vote/03_export.R
+++ scripts/01_anti_incumbent_vote/03_export.R
@@ -1,4 +1,4 @@
 
 # Export -----------------------------------------------------------
 
-save(ned_v_dem_cee, file = here("data", "ned_v_dem_cee.Rda"))
+save(ned_v_dem_cee, file = here("data", "01_anti_incumbent_vote", "ned_v_dem_cee.Rda"))
