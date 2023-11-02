793075e3a9b0330d61f89f38744f60fabead3690
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-11-02 20:37:54 +0100

    Add NUTS3 list to universal variables

--- .Rprofile
+++ .Rprofile
@@ -12,3 +12,4 @@ library(readxl) # Read in .xls files
 library(scales) # This will ensure that numbers on the y-axis are formatted with commas and without scientific notation.
 library(plm) # Panel data analysis
 library(stargazer) # Exporting nice tables
+library(eurostat) # NUTS3 names
--- scripts/00_universal_variables.R
+++ scripts/00_universal_variables.R
@@ -1,11 +1,25 @@
 
-# CEE NUTS0
+# CEE NUTS0 --------------------------------------
+
 cee <- c("BG", "HR", "CZ", "EE", "HU", "LV", "LT", "PL", "RO", "SK", "SI")
 cee_names <- c("Bulgaria", "Croatia", "Czech Republic", "Czechia", "Estonia", "Hungary", "Latvia", "Lithuania", "Poland", "Romania", "Slovakia", "Slovenia")
 
 
-# Croatian NUTS3 Recoding
+# Croatian NUTS3 Recoding --------------------------------------
+
 # https://ec.europa.eu/eurostat/web/nuts/background
 # https://ec.europa.eu/eurostat/documents/345175/629341/NUTS2021.xlsx
 # /Data/NUTS2021.xlsx
 hr_nuts_recoding <- read_csv(here("data", "HR_NUTS_Recoding.csv"))
+
+
+# NUTS3 IDs and Names --------------------------------------
+
+cee_nuts3 <- eurostat::eurostat_geodata_60_2016 %>% 
+  select(NUTS_ID, NAME_LATN, NUTS_NAME) %>% 
+  # Filter for CEE
+  dplyr::filter(str_detect(NUTS_ID, paste0("^", cee, collapse = "|"))) %>% 
+  # Filter for NUTS3
+  dplyr::filter(str_length(NUTS_ID) == 5)
+  
+cee_nuts3$geometry <- NULL
