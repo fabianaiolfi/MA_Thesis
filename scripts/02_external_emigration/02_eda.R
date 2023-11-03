9bb9f30161d998761fee87f4cf53a6bbff291fc6
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-11-03 09:36:48 +0100

    Add plot to EDA

--- scripts/02_external_emigration/02_eda.R
+++ scripts/02_external_emigration/02_eda.R
@@ -2,3 +2,9 @@
 # Exploratory Data Analysis -----------------------------------------------
 
 ## Bulgaria -------------------------------------------------------------
+
+# Plot crude emigration over time
+
+ggplot(bg, aes(x = year, y = crude_emigration, line = NUTS_ID)) +
+  geom_line() +
+  theme_minimal()
