e90bbd4a8410a3c5698352c903ccdb1b06447b84
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-11-10 10:57:13 +0100

    Create Estonian population dataframe

--- scripts/02_external_emigration/01_import_preprocess/ee.R
+++ scripts/02_external_emigration/01_import_preprocess/ee.R
@@ -86,10 +86,10 @@ rm(ee_emigration_00_17, ee_emigration_15_22)
 # 1990---2017
 #RV022: POPULATION, 1 JANUARY by Year, County, Sex and Age group
 # Source: https://andmed.stat.ee/en/stat/Lepetatud_tabelid__Rahvastik.Arhiiv__Rahvastikun%C3%A4itajad%20ja%20koosseis.%20Arhiiv/RV022/table/tableViewLayout2 (retrieved 7 November 2023)
-ee_population_00_17 <- read_csv(here("data", "02_external_emigration", "ee", "RV022_20231107-230654.csv"),
+ee_population_90_17 <- read_csv(here("data", "02_external_emigration", "ee", "RV022_20231107-230654.csv"),
                                 skip = 1) # Skip first row to prevent CSV import mess
 
-ee_population_00_17 <- ee_population_00_17 %>% 
+ee_population_90_17 <- ee_population_90_17 %>% 
   select(-Sex) %>% 
   rename(year = Year,
          countyname = County,
@@ -117,13 +117,42 @@ ee_population_00_17 <- ee_population_00_17 %>%
   group_by(NUTS_ID, year) %>% 
   summarise(population = sum(population))
 
+# 2018--2023
+# RV022U: POPULATION, 1 JANUARY. ADMINISTRATIVE DIVISION AS AT 01.01.2018 by Year, County, Sex and Age group
+# https://andmed.stat.ee/en/stat/rahvastik__rahvastikunaitajad-ja-koosseis__rahvaarv-ja-rahvastiku-koosseis/RV022U/table/tableViewLayout2 (retrieved 7 November 2023)
+ee_population_18_23 <- read_csv(here("data", "02_external_emigration", "ee", "RV022U_20231107-230138.csv"),
+                                skip = 1) # Skip first row to prevent CSV import mess
 
-unique(ee_population_00_17$NUTS_ID)
-unique(ee_population_00_17$year)
-  
-  
-  
-  
+ee_population_18_23 <- ee_population_18_23 %>% 
+  select(-Sex) %>% 
+  rename(year = Year,
+         countyname = County,
+         population = `Age groups total`) %>% 
+  dplyr::filter(countyname != "County unknown") %>% 
+  mutate(NUTS_ID = case_when(countyname == "Harju county" ~ "EE001",
+                             countyname == "Hiiu county" ~ "EE004",
+                             countyname == "Lääne county" ~ "EE004",
+                             countyname == "Pärnu county" ~ "EE004",
+                             countyname == "Saare county" ~ "EE004",
+                             countyname == "Järva county" ~ "EE006",
+                             countyname == "Lääne-Viru county" ~ "EE006",
+                             countyname == "Rapla county" ~ "EE006",
+                             countyname == "Ida-Viru county" ~ "EE007",
+                             countyname == "Jõgeva county" ~ "EE008",
+                             countyname == "Põlva county" ~ "EE008",
+                             countyname == "Tartu county" ~ "EE008",
+                             countyname == "Valga county" ~ "EE008",
+                             countyname == "Viljandi county" ~ "EE008",
+                             countyname == "Võru county" ~ "EE008")) %>% 
+  drop_na(NUTS_ID) %>% 
+  mutate(population = as.numeric(population)) %>% 
+  # Calculate sum by NUTS_ID and year
+  group_by(NUTS_ID, year) %>% 
+  summarise(population = sum(population))
+
+# Merge both sources
+ee_population <- rbind(ee_population_90_17, ee_population_18_23)
+rm(ee_population_90_17, ee_population_18_23)
 
  
 ## Calculate crude emigration --------------------------------
