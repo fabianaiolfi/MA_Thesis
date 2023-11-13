1bb2172ad468c427b4fce74523e777a7423976ee
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-11-13 20:28:00 +0100

    Process RO emigration data

--- scripts/02_external_emigration/01_import_preprocess/ro.R
+++ scripts/02_external_emigration/01_import_preprocess/ro.R
@@ -1,11 +1,11 @@
 
-# Slovakia -------------------------------------------------------------
+# Romania -------------------------------------------------------------
 
 
 ## Emigration -------------------------------------------------------------
 
 # Source: http://statistici.insse.ro:8077/tempo-online/#/pages/tables/insse-table (retrieved 13 November 2023)
-# File must be resaved as from XLS to XLSX
+# Original file must be resaved as from XLS to XLSX
 raw_xl <- read_excel(here("data", "02_external_emigration", "ro", "TEMPO_POP309E_1_10_2023.xlsx"), skip = 2)
  
 ro_emigration <- raw_xl %>% 
@@ -16,71 +16,49 @@ ro_emigration <- raw_xl %>%
   left_join(select(cee_nuts3, NUTS_ID, NAME_LATN), by = c("regionname" = "NAME_LATN"))
 
 # Which regions didn't make it in the join?
-missing_nuts <- ro_emigration %>% select(regionname, NUTS_ID) %>% dplyr::filter(is.na(NUTS_ID) == T)
+# Manuall add them
+#missing_nuts <- ro_emigration %>% select(regionname, NUTS_ID) %>% dplyr::filter(is.na(NUTS_ID) == T)
 
 ro_emigration <- ro_emigration %>% 
-  mutate(NUTS_ID = case_when(regionname == "Arges" ~ " ",
-                             regionname == "Bacau" ~ " ",
-                             regionname == "Bistrita-Nasaud" ~ " ",
-                             regionname == "Botosani" ~ " ",
-                             regionname == "Brasov" ~ " ",
-                             regionname == "Braila" ~ " ",
-                             regionname == "Buzau" ~ " ",
-                             regionname == "Caras-Severin" ~ " ",
-                             regionname == "Calarasi" ~ " ",
-                             regionname == "Constanta" ~ " ",
-                             regionname == "Dambovita" ~ " ",
-                             regionname == "Galati" ~ " ",
-                             regionname == "Ialomita" ~ " ",
-                             regionname == "Iasi" ~ " ",
-                             regionname == "Maramures" ~ " ",
-                             regionname == "Mehedinti" ~ " ",
-                             regionname == "Mures" ~ " ",
-                             regionname == "Neamt" ~ " ",
-                             regionname == "Salaj" ~ " ",
-                             regionname == "Timis" ~ " ",
-                             regionname == "Valcea" ~ " ",
-                             regionname == "Bucharest Municipality" ~ " "))
-
-# sk_emigration <- raw_xl %>% 
-#   rename(regionname = `...1`) %>% 
-#   dplyr::filter(str_detect(regionname, "^Region of")) %>% 
-#   dplyr::filter(!str_detect(regionname, "NUTS2")) %>% 
-#   mutate(NUTS_ID = case_when(regionname == "Region of Bratislava" ~ "SK010",
-#                              regionname == "Region of Trnava" ~ "SK021",
-#                              regionname == "Region of Trenčín" ~ "SK022",
-#                              regionname == "Region of Nitra" ~ "SK023",
-#                              regionname == "Region of Žilina" ~ "SK031",
-#                              regionname == "Region of Banská Bystrica" ~ "SK032",
-#                              regionname == "Region of Prešov" ~ "SK041",
-#                              regionname == "Region of Košice" ~ "SK042")) %>% 
-#   select(-regionname) %>% 
-#   pivot_longer(cols = -NUTS_ID,
-#                names_to = "year",
-#                values_to = "emigration")
+  mutate(NUTS_ID = case_when(regionname == "Arges" ~ "RO311",
+                             regionname == "Bacau" ~ "RO211",
+                             regionname == "Bistrita-Nasaud" ~ "RO112",
+                             regionname == "Botosani" ~ "RO212",
+                             regionname == "Brasov" ~ "RO122",
+                             regionname == "Braila" ~ "RO221",
+                             regionname == "Buzau" ~ "RO222",
+                             regionname == "Caras-Severin" ~ "RO422",
+                             regionname == "Calarasi" ~ "RO312",
+                             regionname == "Constanta" ~ "RO223",
+                             regionname == "Dambovita" ~ "RO313",
+                             regionname == "Galati" ~ "RO224",
+                             regionname == "Ialomita" ~ "RO315",
+                             regionname == "Iasi" ~ "RO213",
+                             regionname == "Maramures" ~ "RO114",
+                             regionname == "Mehedinti" ~ "RO413",
+                             regionname == "Mures" ~ "RO125",
+                             regionname == "Neamt" ~ "RO214",
+                             regionname == "Salaj" ~ "RO116",
+                             regionname == "Timis" ~ "RO424",
+                             regionname == "Valcea" ~ "RO415",
+                             regionname == "Bucharest Municipality" ~ "RO321",
+                             T ~ NUTS_ID)) %>% 
+  select(-regionname) %>% 
+  pivot_longer(cols = -NUTS_ID,
+               names_to = "year",
+               values_to = "emigration") %>% 
+  mutate(year = gsub("Year ", "", year)) %>% 
+  mutate(year = as.numeric(year),
+         emigration = as.numeric(emigration))
 
 
 ## Population -------------------------------------------------------------
 
-# Source: https://datacube.statistics.sk/#!/view/en/vbd_dem/om7011rr/v_om7011rr_00_00_00_en (retrieved 10 November 2023)
-raw_xl <- read_excel(here("data", "02_external_emigration", "sk", "v_om7011rr_00_00_00_en20231110074721.xlsx"), skip = 5)
+# Source: http://statistici.insse.ro:8077/tempo-online/#/pages/tables/insse-table (retrieved 13 November 2023)
+# Original file must be resaved as from XLS to XLSX
+raw_xl <- read_excel(here("data", "02_external_emigration", "ro", "TEMPO_POP107A_1_10_2023.xls"))#, skip = 5)
 
-sk_population <- raw_xl %>% 
-  rename(regionname = `...1`) %>% 
-  dplyr::filter(str_detect(regionname, "^Region of")) %>% 
-  dplyr::filter(!str_detect(regionname, "NUTS2")) %>% 
-  mutate(NUTS_ID = case_when(regionname == "Region of Bratislava" ~ "SK010",
-                             regionname == "Region of Trnava" ~ "SK021",
-                             regionname == "Region of Trenčín" ~ "SK022",
-                             regionname == "Region of Nitra" ~ "SK023",
-                             regionname == "Region of Žilina" ~ "SK031",
-                             regionname == "Region of Banská Bystrica" ~ "SK032",
-                             regionname == "Region of Prešov" ~ "SK041",
-                             regionname == "Region of Košice" ~ "SK042")) %>% 
-  select(-regionname) %>% 
-  pivot_longer(cols = -NUTS_ID,
-               names_to = "year",
-               values_to = "population")
+
 
 
 ## Calculate crude emigration --------------------------------
