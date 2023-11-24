3208479a3760128f3260767ce2f04e70a426b032
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-11-24 17:24:26 +0100

    Update script

--- scripts/03_service_cuts/01_import_preprocess/pl.R
+++ scripts/03_service_cuts/01_import_preprocess/pl.R
@@ -162,6 +162,7 @@ save(pl_schools, file = here("data", "03_service_cuts", "pl", "pl_schools.Rda"))
 
 # Hospitals ------------------------------
 
+# Hospital Beds
 raw_csv <- read_csv(here("data", "03_service_cuts", "pl", "hlth_rs_bdsrg__custom_8678583_linear.csv"))
 
 pl_hospitals <- raw_csv %>% 
@@ -178,7 +179,41 @@ pl_hospitals <- raw_csv %>%
                              NUTS_ID == "PL34" ~ "PL84",
                              T ~ NUTS_ID))
 
-load(here("data", "02_external_emigration", "pl", "pl_population.Rda"))
+# Hospitals
+# Source: https://bdl.stat.gov.pl/bdl/dane/podgrup/tablica (retrieved 24 November 2023)
+raw_csv <- read_delim(here("data", "03_service_cuts", "pl", "OCHR_2452_CTAB_20231124171753.csv"),
+                      delim = ";")
+
+pl_hospitals <- raw_csv %>% 
+  select(-c(Code, `...22`)) %>% 
+  rename(regionname = Name)
+
+# Rename columns to year
+year_names <- as.character(2004:2022)
+current_names <- colnames(pl_hospitals) # Getting the current column names
+current_names[2:20] <- year_names # Replacing the names of columns 2 through 29
+colnames(pl_hospitals) <- current_names # Assigning the new names back to the dataframe
+#pl_hospitals <- pl_hospitals %>% select(-c(`1995`, `1996`, `1997`))
+
+# More preprocessing
+pl_hospitals <- pl_hospitals %>% 
+  mutate(regionname = sapply(regionname, capitalizeAfterSpace)) %>%
+  left_join(select(cee_nuts2, NUTS_ID, NAME_LATN), by = c("regionname" = "NAME_LATN")) %>% 
+  mutate(NUTS_ID = case_when(regionname == "Mazowieckie" ~ "PL92",
+                             regionname == "City With Powiat Status Capital City Warszawa" ~ "PL91",
+                             T ~ NUTS_ID)) %>%
+  select(-regionname) %>% 
+  pivot_longer(cols = -NUTS_ID,
+               names_to = "year",
+               values_to = "hospitals") %>% 
+  mutate(year = as.numeric(year))
+
+ggplot(pl_hospitals, aes(x = year, y = hospitals, line = NUTS_ID)) +
+  geom_line() +
+  theme_minimal()
+
+
+# load(here("data", "02_external_emigration", "pl", "pl_population.Rda"))
 
 # pl_hospitals <- pl_hospitals %>% 
 #   left_join(pl_population, by = c("year", "NUTS_ID")) %>% 
@@ -196,7 +231,7 @@ raw_csv <- read_delim(here("data", "03_service_cuts", "pl", "LUDN_2137_CTAB_2023
 
 pl_population_over_70 <- raw_csv %>% 
   select(-c(Code, `...31`)) %>% 
-  rename(regionname = Name)# %>% 
+  rename(regionname = Name) 
   
 # Rename columns to year
 year_names <- as.character(1995:2022)
@@ -227,6 +262,7 @@ pl_hospitals <- pl_hospitals %>%
 ggplot(pl_hospitals, aes(x = year, y = ratio_hospital_beds_population_over_70, line = NUTS_ID)) +
   geom_line() +
   theme_minimal()
+
 
 ## Export ------------------------------
 
