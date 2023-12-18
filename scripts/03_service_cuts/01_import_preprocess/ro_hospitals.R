b500bb0c5586696285aed7f7977db7858549574b
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2023-12-18 13:54:31 +0100

    Replace data source

--- scripts/03_service_cuts/01_import_preprocess/ro_hospitals.R
+++ scripts/03_service_cuts/01_import_preprocess/ro_hospitals.R
@@ -3,12 +3,12 @@
 
 ## Hospitals at NUTS3 --------------------------------
 
-# Source: http://statistici.insse.ro:8077/tempo-online/#/pages/tables/insse-table (Retrieved 9 October 2023)
-raw_data <- read_excel(here("data", "03_service_cuts", "ro", "TEMPO_SAN102C_9_10_2023.xlsx")) # Manually converted .xls to .xlsx
+# Source: http://statistici.insse.ro:8077/tempo-online/#/pages/tables/insse-table (Retrieved 18 December 2023)
+raw_data <- read_excel(here("data", "03_service_cuts", "ro", "TEMPO_SAN101A_18_12_2023.xlsx")) # Manually converted .xls to .xlsx
 
 # Hospital Beds
 ro_hospitals <- raw_data %>% 
-  select(-c(1, 2, 4))
+  select(-c(1, 2))
 
 year_names <- as.character(1990:2022)
 current_names <- colnames(ro_hospitals) # Getting the current column names
@@ -18,18 +18,20 @@ colnames(ro_hospitals) <- current_names # Assigning the new names back to the da
 ned_v_dem_ro <- ned_v_dem_cee %>% select(country, regionname, nuts2016) %>% dplyr::filter(country == "Romania") %>% distinct(regionname, .keep_all = T) %>% select(-country)
 
 ro_hospitals <- ro_hospitals %>% 
-  slice(5:n()) %>% 
+  slice(8:n()) %>% 
   rename(region = `...3`) %>% 
   drop_na(region) %>% 
   mutate(region = case_when(region == "Bucharest Municipality" ~ "Bucuresti",
+                            region == "Bucharest Municipality - including SAI" ~ "Bucuresti",
                             T ~ region)) %>% 
   left_join(ned_v_dem_ro, by = c("region" = "regionname")) %>%
   select(-region) %>% 
   pivot_longer(cols = -nuts2016,
                names_to = "year",
-               values_to = "hospitals") %>% 
+               values_to = "hospitals") %>%
   mutate(year = as.numeric(year)) %>% 
-  mutate(hospitals = as.numeric(hospitals))
+  mutate(hospitals = as.numeric(hospitals)) %>% 
+  drop_na(hospitals)
 
 
 ## Hospitals Merge ------------------------------
