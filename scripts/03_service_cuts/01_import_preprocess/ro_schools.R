e098428c64869d1017b0245a58bbab0873595aad
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-01-08 10:06:42 +0100

    Add number of classrooms and pupils per classroom ratio to descriptive stats

--- scripts/03_service_cuts/01_import_preprocess/ro_schools.R
+++ scripts/03_service_cuts/01_import_preprocess/ro_schools.R
@@ -67,12 +67,47 @@ ro_schools_pop <- ro_schools_pop %>%
   drop_na(population)
 
 
+## Classrooms ------------------------------
+
+# Source: http://statistici.insse.ro:8077/tempo-online/#/pages/tables/insse-table (Retrieved 8 January 2024)
+raw_data <- read_excel(here("data", "03_service_cuts", "ro", "TEMPO_SCL105C_8_1_2024.xlsx")) # Manually converted .xls to .xlsx
+
+# Classrooms
+ro_classrooms <- raw_data %>% 
+  select(c(`...2`:`...35`))
+
+year_names <- as.character(1990:2022)
+current_names <- colnames(ro_classrooms) # Getting the current column names
+current_names[2:34] <- year_names # Replacing the names of columns 2 through 29
+colnames(ro_classrooms) <- current_names # Assigning the new names back to the dataframe
+
+ro_classrooms <- ro_classrooms %>% 
+  slice(8:n()) %>% 
+  rename(region = `...2`) %>% 
+  drop_na(region) %>% 
+  dplyr::filter(!region %in% ro_nuts2) %>% 
+  mutate(across(everything(), ~gsub(':', NA, .))) %>% 
+  mutate(region = case_when(region == "Bucharest Municipality - including SAI" ~ "Bucuresti",
+                            region == "Bucharest Municipality" ~ "Bucuresti",
+                            T ~ region)) %>% 
+  left_join(ned_v_dem_ro, by = c("region" = "regionname")) %>% 
+  select(-region) %>% 
+  pivot_longer(cols = -nuts2016,
+               names_to = "year",
+               values_to = "classrooms") %>% 
+  mutate(year = as.numeric(year)) %>% 
+  mutate(classrooms = as.numeric(classrooms)) %>% 
+  drop_na(classrooms)
+
+
 ## Schools Merge ------------------------------
 
 ro_schools <- ro_schools %>% 
   left_join(ro_schools_pop, by = c("nuts2016", "year")) %>% 
+  left_join(ro_classrooms, by = c("nuts2016", "year")) %>% 
   distinct(nuts2016, year, .keep_all = T) %>% 
   mutate(ratio_schools = population / schools) %>% 
+  mutate(ratio_classrooms = population / classrooms) %>% 
   rename(NUTS_ID = nuts2016)
 
 
