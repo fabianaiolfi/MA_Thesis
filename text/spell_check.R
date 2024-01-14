7a72ff802f526b32cea64a36696f9b4cfbc350d6
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-01-14 20:20:47 +0100

    Update helper scripts

--- text/spell_check.R
+++ text/spell_check.R
@@ -1,7 +1,7 @@
 
 # Get all file names
 
-main_folder_path <- here("text", "data_analysis")
+main_folder_path <- here("text", "thesis")
 
 # Create a vector of all Rmd file names
 rmd_files <- list.files(path = main_folder_path, 
@@ -12,4 +12,4 @@ rmd_files <- list.files(path = main_folder_path,
 # Remove 06_sources.Rmd
 rmd_files <- rmd_files[-14]
 
-spelling::spell_check_files(rmd_files, lang = "en-GB", ignore = c("CEE", "CHES", "pagebreak", "RO", "PL", "Czechia", "Voivodeship", "uptick"))
+spelling::spell_check_files(rmd_files, lang = "en-GB", ignore = c("CEE", "CHES", "pagebreak", "RO", "PL", "Czechia", "Voivodeship", "uptick", "operationalisation"))
