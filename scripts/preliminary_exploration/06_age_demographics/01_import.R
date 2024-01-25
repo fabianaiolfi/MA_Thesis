d5bd1fe9a91ab3571fc3468524cb18596b138a82
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-01-25 21:15:17 +0100

    Update thesis

--- scripts/preliminary_exploration/06_age_demographics/01_import.R
+++ scripts/preliminary_exploration/06_age_demographics/01_import.R
@@ -7,7 +7,8 @@ pattern <- paste(cee, collapse = "|")
 
 # Data Import and Preprocessing -------------------------------------------------------------
 
-df <- read.csv(here("data", "demo_r_pjanaggr3_linear.csv"))
+demo_r_pjanaggr3_linear <- read.csv(here("data", "demo_r_pjanaggr3_linear.csv"))
+# save(demo_r_pjanaggr3_linear, file = here("data", "demo_r_pjanaggr3_linear.rda"))
 
 # NUTS3
 # df <- df %>% 
@@ -18,16 +19,16 @@ df <- read.csv(here("data", "demo_r_pjanaggr3_linear.csv"))
 #   dplyr::filter(str_length(geo) == 5)
 
 # NUTS0
-df <- df %>% 
+demo_r_pjanaggr3_linear <- demo_r_pjanaggr3_linear %>% 
   select(sex, age, geo, TIME_PERIOD, OBS_VALUE) %>% 
   dplyr::filter(geo %in% cee) %>% 
   dplyr::filter(sex == "T") %>% 
   select(-sex)
 
 # Less than 15 years [Y_LT15]
-df_young <- df %>% 
+df_young <- demo_r_pjanaggr3_linear %>% 
   dplyr::filter(age == "Y_LT15")
 
 # 65 years or over [Y_GE65]
-df_old <- df %>% 
+df_old <- demo_r_pjanaggr3_linear %>% 
   dplyr::filter(age == "Y_GE65")
