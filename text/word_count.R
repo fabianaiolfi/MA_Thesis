cacd8100f6ef571d7de4cac11fc4b36997f10e6d
Fabian Aiolfi <fabian.aiolfi@gmail.com>
2024-02-01 22:54:48 +0100

    Update script

--- text/word_count.R
+++ text/word_count.R
@@ -70,9 +70,9 @@ folder_path <- "text/thesis"
 total_words <- count_words_in_rmd(folder_path)
 cat("Total words in all Rmd files:", total_words)
 
-# remove tables, e.g. in overview.Rmd?
 
-# 240131: 11'155| 11'149
+# 240201: 11'382
+# 240131: 11'155
 # 240130: 10'850
 # 240129: 10'395
 # 240128: 10'172
